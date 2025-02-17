18b4450ee40e3227562a1dc46c3bf2cb2179422a
Joel Kallstrom <joelkallstrom@outlook.com>
2025-02-17 22:48:14 +0000

    adds images

--- src/app/components/ProjectCard.tsx
+++ src/app/components/ProjectCard.tsx
@@ -9,6 +9,7 @@ export default function ProjectCard({ project }: ProjectCardProps) {
     <div className="max-w-sm rounded overflow-hidden shadow-lg bg-white text-black m-4">
       <h2 className="text-2xl font-semibold mb-2 p-4">{project.title}</h2>
       <p className="p-4">{project.description}</p>
+      <img src={project.image} alt={project.title} />
       <a
         href={project.link}
         className="m-4 block bg-blue-500 text-white px-4 py-2 rounded hover:bg-blue-600 text-center"
