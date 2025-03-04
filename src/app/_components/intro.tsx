16a328b95eb428373ebd5e4bed5f88a2dc0040ab
Joel Kallstrom <joelkallstrom@outlook.com>
2025-03-04 23:05:46 +0000

    Update intro.tsx

--- src/app/_components/intro.tsx
+++ src/app/_components/intro.tsx
@@ -1,20 +1,21 @@
 import { CMS_NAME } from "@/lib/constants";
 
-export function Intro() {
+export function Intro({
+  username = "Data Engineer",
+  introText = "I build data pipelines and analytics solutions",
+}) {
   return (
-    <section className="flex-col md:flex-row flex items-center md:justify-between mt-16 mb-16 md:mb-12">
-      <h1 className="text-5xl md:text-8xl font-bold tracking-tighter leading-tight md:pr-8">
-        Blog.
-      </h1>
-      <h4 className="text-center md:text-left text-lg mt-5 md:pl-8">
-        A statically generated blog example using{" "}
-        <a
-          href="https://nextjs.org/"
-          className="underline hover:text-blue-600 duration-200 transition-colors"
-        >
-          Next.js
-        </a>{" "}
-        and {CMS_NAME}.
+    <section className="flex flex-col md:flex-row items-center md:justify-between mt-16 mb-16 md:mb-12">
+      <div className="flex flex-col md:mr-8">
+        <h1 className="text-5xl md:text-8xl font-bold tracking-tighter leading-tight">
+          Portfolio
+        </h1>
+        <h2 className="text-2xl md:text-4xl font-bold tracking-tight md:tracking-tighter leading-tight mb-8 md:mb-12">
+          {username}
+        </h2>
+      </div>
+      <h4 className="text-center md:text-left text-lg mt-5 md:mt-0 md:pl-8">
+        {introText}
       </h4>
     </section>
   );
