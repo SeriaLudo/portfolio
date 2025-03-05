65786d16cf8af65d9a42306d38d21c9c2547edd7
Joel Kallstrom <joelkallstrom@outlook.com>
2025-03-05 21:29:08 +0000

    adds basePath

--- next.config.js
+++ next.config.js
@@ -1,5 +1,6 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
+  basePath: "/nextjs-blog",
   output: "export", // Enables `next export`
   images: {
     unoptimized: true, // Required if you're using the Next.js Image component
@@ -7,4 +8,3 @@ const nextConfig = {
 };
 
 module.exports = nextConfig;
-
