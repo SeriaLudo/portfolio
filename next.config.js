2813c2ecf9116a7348341d74da9ca4edfe3d0f07
Joel Kallstrom <joelkallstrom@outlook.com>
2025-03-05 22:24:22 +0000

    adds assetPrefix

--- next.config.js
+++ next.config.js
@@ -1,6 +1,7 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   basePath: "/nextjs-blog",
+  assetPrefix: "/nextjs-blog/",
   output: "export", // Enables `next export`
   images: {
     unoptimized: true, // Required if you're using the Next.js Image component
