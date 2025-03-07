268363f628860845e999c89be0ce27ca15623ea1
Joel Kallstrom <joelkallstrom@outlook.com>
2025-03-07 00:49:42 +0000

    adds dompurify

--- src/app/_components/post-body.tsx
+++ src/app/_components/post-body.tsx
@@ -1,15 +1,19 @@
 import markdownStyles from "./markdown-styles.module.css";
+import DOMPurify from "isomorphic-dompurify";
 
 type Props = {
   content: string;
 };
 
 export function PostBody({ content }: Props) {
+  // Use DOMPurify to sanitize the HTML content
+  const sanitizedContent = DOMPurify.sanitize(content);
+
   return (
     <div className="max-w-2xl mx-auto">
       <div
         className={markdownStyles["markdown"]}
-        dangerouslySetInnerHTML={{ __html: content }}
+        dangerouslySetInnerHTML={{ __html: sanitizedContent }}
       />
     </div>
   );
