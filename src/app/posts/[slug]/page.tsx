fafc7795badd934b000b2206faaf3f9b1e342dea
Joel Kallstrom <joelkallstrom@outlook.com>
2025-03-10 14:44:43 +0000

    fixes breaking change

--- src/app/posts/[slug]/page.tsx
+++ src/app/posts/[slug]/page.tsx
@@ -1,17 +1,17 @@
 import { Metadata } from "next";
 import { notFound } from "next/navigation";
 import { getAllPosts, getPostBySlug } from "../../../lib/api";
-import { CMS_NAME } from "../../../lib/constants";
 import markdownToHtml from "../../../lib/markdownToHtml";
-import Alert from "../../_components/alert";
 import Container from "../../_components/container";
 import Header from "../../_components/header";
 import { PostBody } from "../../_components/post-body";
 import { PostHeader } from "../../_components/post-header";
-import Link from "next/link";
 
-export default async function Post({ params }: Params) {
-  const post = getPostBySlug(params.slug);
+type Params = Promise<{ slug: string }>;
+
+export default async function Page({ params }: { params: Params }) {
+  const { slug } = await params; // ✅ Await params because it's now async
+  const post = getPostBySlug(slug);
 
   if (!post) {
     return notFound();
@@ -21,7 +21,6 @@ export default async function Post({ params }: Params) {
 
   return (
     <main>
-      {/* <Alert preview={post.preview} /> */}
       <Container>
         <Header />
         <article className="mb-32">
@@ -37,31 +36,27 @@ export default async function Post({ params }: Params) {
   );
 }
 
-type Params = {
-  params: {
-    slug: string;
-  };
-};
-
-export function generateMetadata({ params }: Params): Metadata {
-  const post = getPostBySlug(params.slug);
+export async function generateMetadata({
+  params,
+}: {
+  params: Params;
+}): Promise<Metadata> {
+  const { slug } = await params; // ✅ Await params
+  const post = getPostBySlug(slug);
 
   if (!post) {
     return notFound();
   }
 
-  const title = `${post.title}`;
-
   return {
     openGraph: {
-      title,
+      title: post.title,
     },
   };
 }
 
 export async function generateStaticParams() {
   const posts = getAllPosts();
-
   return posts.map((post) => ({
     slug: post.slug,
   }));
